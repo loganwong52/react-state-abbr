f3a48c3f59b00fb742acc6fab76f0087265d36fc
loganwong52 <loganwong52@gmail.com>
2022-07-10 12:26:42 -0700

    completed release 0 and 1

--- src/App.js
+++ src/App.js
@@ -1,20 +1,46 @@
 import React from 'react';
 import './App.css';
-import './statedata/index'
+import UsaStateData from './statedata/data'
 // index.js in statedata is a list of objects
+import { useState } from 'react'
 
 function App() {
+  // state values
+  // create these when the data changing impacts the rendering
+  const [stateAbbr, setStateAbbr] = useState("")
+
+  // event handlers
+  const onSelectState = (evt) => {
+    // print out the selected state's abbr
+    // console.log(evt.currentTarget.value)
+
+    setStateAbbr(evt.currentTarget.value)
+    // console.log(stateAbbr)    // this lags behind 1
+  }
+
+  //render
+  const populateStateDropdown = () => {
+    let elements = []
+    for (let i = 0; i < UsaStateData.length; i++) {
+      elements.push(
+        <option key={`state-${i}`} value={UsaStateData[i]["alpha-2"]}>
+          {UsaStateData[i]["name"]}
+        </option>
+      )
+    }
+    return elements
+  }
+
   return (
     <div className="App">
       <h1>State Abbreviation App</h1>
       <hr />
-      <select>
-        <option value="1">Donuts</option>
-        <option value="2">Bagels</option>
-        <option value="3">Peach Ring</option>
+      {/* pass in function, but not calling it right away */}
+      <select onChange={onSelectState}>
+        {populateStateDropdown()}
       </select>
       <div id="div-state-abbr">
-        CA
+        {stateAbbr}
       </div>
     </div>
   );
