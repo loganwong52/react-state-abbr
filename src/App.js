1cf3617eef2341e15170b8b9239b605541469b79
loganwong52 <loganwong52@gmail.com>
2022-07-10 09:56:50 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import './statedata/index'
+// index.js in statedata is a list of objects
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>State Abbreviation App</h1>
+      <hr />
+      <select>
+        <option value="1">Donuts</option>
+        <option value="2">Bagels</option>
+        <option value="3">Peach Ring</option>
+      </select>
+      <div id="div-state-abbr">
+        CA
+      </div>
     </div>
   );
 }
